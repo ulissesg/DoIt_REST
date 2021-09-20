fe6d9c649015da0086dfa908114958e498ec211c
ulissesg <ulissesg2018@gmail.com>
2021-09-20 15:02:31 -0300

    initial commit

---
+++ DoIt_REST/DoIt/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class List(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400, blank=True, null=True)
+    is_done = models.BooleanField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    time_it_takes = models.IntegerField('How much time it takes in minutes', blank=True, null=True)
+    is_important = models.BooleanField(blank=True, null=True)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
